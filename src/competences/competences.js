6119785007a21859f1ed3be5f01c42865d1b1f7b
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2016-06-30 23:49:56 +0200

    competences public

--- src/competences/competences.js
+++ src/competences/competences.js
@@ -5,7 +5,7 @@ angular.module('angular-login.competences', ['angular-login.grandfather'])
                 url: '/competences',
                 templateUrl: 'competences/competences.tpl.html',
                 controller: 'CompetencesController',
-                accessLevel: accessLevels.admin
+                accessLevel: accessLevels.public
             });
     })
     .controller('CompetencesController', function ($scope, loginService, $http, $log) {
