78b98fb8145c3c26526876bdadbf58277648cef3
Sébastien Lavayssiere <slavayssiere@fastbooking.net>
2016-07-12 11:13:22 +0200

    add stats pages

--- src/stats/stats.js
+++ src/stats/stats.js
@@ -7,9 +7,41 @@ angular.module('angular-login.stats', ['angular-login.grandfather'])
       controller: 'StatsController',
       accessLevel: accessLevels.dlaf
     })
+    .state('app.statsmaraude', {
+      url: '/statsmaraude',
+      templateUrl: 'stats/statsmaraude.tpl.html',
+      controller: 'StatsMaraudeController',
+      accessLevel: accessLevels.public
+    })
+    .state('app.statsfc', {
+      url: '/statsfc',
+      templateUrl: 'stats/statsfc.tpl.html',
+      controller: 'StatsFCController',
+      accessLevel: accessLevels.dlaf
+    })
 })
 .controller('StatsController', function ($scope, loginService, $http, $log) {
     var url_search = 'http://'+$scope.url+'/stats/formations?F5_ST='+loginService.user.F5_ST+'&LastMRH_Session='+loginService.user.LastMRH_Session+'&MRHSession='+loginService.user.MRHSession+'&ul='+loginService.user.utilisateur.structure.id;
+    $log.info('URI: ' + url_search);
+
+    $http.get(url_search).
+        success(function(response){
+        $scope.data = angular.fromJson(response);
+        }
+    );         
+})
+.controller('StatsMaraudeController', function ($scope, loginService, $http, $log) {
+    var url_search = 'http://'+$scope.url+'/stats/maraude?F5_ST='+loginService.user.F5_ST+'&LastMRH_Session='+loginService.user.LastMRH_Session+'&MRHSession='+loginService.user.MRHSession+'&ul='+loginService.user.utilisateur.structure.id;
+    $log.info('URI: ' + url_search);
+
+    $http.get(url_search).
+        success(function(response){
+        $scope.data = angular.fromJson(response);
+        }
+    );         
+})
+.controller('StatsFCController', function ($scope, loginService, $http, $log) {
+    var url_search = 'http://'+$scope.url+'/stats/fc?F5_ST='+loginService.user.F5_ST+'&LastMRH_Session='+loginService.user.LastMRH_Session+'&MRHSession='+loginService.user.MRHSession+'&ul='+loginService.user.utilisateur.structure.id;
     $log.info('URI: ' + url_search);
 
     $http.get(url_search).
