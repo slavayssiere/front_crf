d1b1c12888d7e04a23bc495cbdb2dcd458e79fb4
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2016-07-31 12:17:26 +0200

    add heroku server

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 //var url_ws_pegass = "localhost:4567/v1";
 var url_ws_pegass = "ws-pegass-prd.ucps6xmicn.eu-west-1.elasticbeanstalk.com/v1";
 
-var url_ws_google = "localhost:8081/api"
+//var url_ws_google = "localhost:8081/api"
+var url_ws_google = "ws-google.herokuapp.com/api"
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+bower install
 npm version patch
 ./node_modules/grunt-cli/bin/grunt build
 aws s3 cp build s3://ul-management/ --recursive
