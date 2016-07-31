91de5bfa1c8df8e7f6deea2fdf075b5682de28a2
Sebastien Lavayssiere <sebastien.lavayssiere@gmail.com>
2016-07-31 17:20:38 +0200

    change default path to ws google

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 var url_ws_pegass = "ws-pegass-prd.ucps6xmicn.eu-west-1.elasticbeanstalk.com/v1";
 
 //var url_ws_google = "localhost:8081/api"
-var url_ws_google = "ws-google.herokuapp.com/api"
+var url_ws_google = "ws-google-prd.cv3d2dj5et.eu-west-1.elasticbeanstalk.com/api"
