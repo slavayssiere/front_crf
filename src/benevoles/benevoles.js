f8637d4b839b6c6494eeb4fb64a7d4f17a10477b
Sébastien Lavayssiere <slavayssiere@fastbooking.net>
2016-08-17 16:58:35 +0200

    solve bug with directory

--- src/benevoles/benevoles.js
+++ src/benevoles/benevoles.js
@@ -9,7 +9,7 @@ angular.module('angular-login.benevoles', ['angular-login.grandfather'])
             })
             .state('app.benevolesadmin', {
                 url: '/benevolesadmin',
-                templateUrl: 'benevole/benevolesadmin.tpl.html',
+                templateUrl: 'benevoles/benevolesadmin.tpl.html',
                 controller: 'BenevolesAdminController',
                 controllerAs: 'benadmin',
                 accessLevel: accessLevels.user
@@ -17,7 +17,7 @@ angular.module('angular-login.benevoles', ['angular-login.grandfather'])
     })
     .controller('BenevolesController', function ($scope, loginService, $http, $log, BenevolesDataFactory) {
 
-        $scope.displayEmails=false;
+        $scope.displayEmails = false;
 
         var url_search = 'http://' + $scope.url + '/benevoles?F5_ST=' + loginService.user.F5_ST + '&LastMRH_Session=' + loginService.user.LastMRH_Session + '&MRHSession=' + loginService.user.MRHSession + '&ul=' + loginService.user.utilisateur.structure.id;
         $log.info('URI: ' + url_search);
@@ -27,22 +27,21 @@ angular.module('angular-login.benevoles', ['angular-login.grandfather'])
                 $scope.data = angular.fromJson(response);
             });
 
-        $scope.getEmails = function(){
+        $scope.getEmails = function () {
             $scope.searching = true;
-            BenevolesDataFactory.searchemail($scope.data).then(function(coordonees){
+            BenevolesDataFactory.searchemail($scope.data).then(function (coordonees) {
                 for (i = 0; i < coordonees.list.length; i++) {
-                    $scope.data.list[i]['coordonees']=coordonees.list[i];
+                    $scope.data.list[i]['coordonees'] = coordonees.list[i];
                 }
-                $scope.displayEmails=true;
+                $scope.displayEmails = true;
                 $scope.searching = false;
                 $scope.emails = BenevolesDataFactory.parseemails();
-            }, function(msg){
+            }, function (msg) {
                 $log.info(msg);
                 $scope.searching = false;
             });
         };
     })
-    
     .controller('BenevolesAdminController', function ($scope, loginService, $http, $log, NgTableParams) {
 
         var self = this;
@@ -51,54 +50,53 @@ angular.module('angular-login.benevoles', ['angular-login.grandfather'])
         $log.info('URI: ' + url_search);
 
         $http.get(url_search + '&page=0').
-                success(function (response) {
-                    $scope.data = angular.fromJson(response);
-                    if ($scope.data.pages > 1) {
-                        for (i = 1; i != $scope.data.pages; i++) {
-                            $http.get(url_search + '&page=' + i).
-                                success(function (res) {
-                                    block = angular.fromJson(res);
-                                    $scope.data.list = $scope.data.list.concat(block.list);
-                                                                                        
-                                    self.tablelist = new NgTableParams(
-                                        {
-                                            sorting: { nom: "asc" },
-                                            page: 1,
-                                            count: $scope.data.list.length
-                                        },{
-                                            counts: [], // hide page counts control
-                                            total: 1,  // value less than count hide pagination
-                                            dataset: $scope.data.list
-                                        }
-                                    );
-                                });
-                        }
+            success(function (response) {
+                $scope.data = angular.fromJson(response);
+                if ($scope.data.pages > 1) {
+                    for (i = 1; i != $scope.data.pages; i++) {
+                        $http.get(url_search + '&page=' + i).
+                            success(function (res) {
+                                block = angular.fromJson(res);
+                                $scope.data.list = $scope.data.list.concat(block.list);
+
+                                self.tablelist = new NgTableParams(
+                                    {
+                                        sorting: { nom: "asc" },
+                                        page: 1,
+                                        count: $scope.data.list.length
+                                    }, {
+                                        counts: [], // hide page counts control
+                                        total: 1,  // value less than count hide pagination
+                                        dataset: $scope.data.list
+                                    }
+                                );
+                            });
                     }
-                });
-                
-            $scope.change = function (person, type){
-            
-            var url_search = 'http://' + $scope.url + '/benevoles/changeinfo/'+person.id+'?F5_ST=' + loginService.user.F5_ST + '&LastMRH_Session=' + loginService.user.LastMRH_Session + '&MRHSession=' + loginService.user.MRHSession;
+                }
+            });
+
+
+        $scope.change = function (person, type) {
+
+            var url_search = 'http://' + $scope.url + '/benevoles/changeinfo/' + person.id + '?F5_ST=' + loginService.user.F5_ST + '&LastMRH_Session=' + loginService.user.LastMRH_Session + '&MRHSession=' + loginService.user.MRHSession;
             var data_struct = {}
-            
-            if(type=="email")
-            {
+
+            if (type == "email") {
                 data_struct = {
                     inscriptionsExternes: person.allow_external,
                     contactParMail: !person.allow_email,
                     mailMoyenComId: person.mailMoyenComId
                 }
-                
+
                 person.allow_email = !person.allow_email;
             }
-            else
-            {
+            else {
                 data_struct = {
                     inscriptionsExternes: !person.allow_external,
                     contactParMail: person.allow_email,
                     mailMoyenComId: person.mailMoyenComId
                 }
-                
+
                 person.allow_external = !person.allow_external;
             }
             var req = {
@@ -109,11 +107,11 @@ angular.module('angular-login.benevoles', ['angular-login.grandfather'])
 
             $http(req).
                 success(function (response) {
-                        angular.forEach($scope.data.list, function(ben, key) {
-                            if(ben.id == person.id){
-                                $scope.data.list[key] = person;
-                            }
-                        });
+                    angular.forEach($scope.data.list, function (ben, key) {
+                        if (ben.id == person.id) {
+                            $scope.data.list[key] = person;
+                        }
+                    });
                 });
         };
     });
